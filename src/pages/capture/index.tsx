d05fabbbadf95808e2bc7583dceb9a796fa34729
Sanyam Arya <er.sanyam.arya@gmail.com>
2021-07-04 10:56:35 +0200

    capture pretty

--- src/pages/capture/index.tsx
+++ src/pages/capture/index.tsx
@@ -1,14 +1,15 @@
-import { Box, Button, Typography, Slider } from '@material-ui/core'
-import { PhotoCameraRounded, SendOutlined } from '@material-ui/icons'
-import React, { ReactElement, useState } from 'react'
-import useStyles from './styles'
+import { Button, IconButton, Slider, Typography } from '@material-ui/core'
+import { DeleteSweep, PhotoCameraRounded, SendOutlined } from '@material-ui/icons'
 import { useNavigate } from '@reach/router'
+import React, { ReactElement, useState } from 'react'
 import CanvasDraw from 'react-canvas-draw'
+import useStyles from './styles'
 
 export default function Capture(): ReactElement {
   const classes = useStyles()
   const [source, setSource] = useState('')
-  const CanvasRef = React.useRef()
+  const [brushSize, setBrushSize] = useState<number | string | Array<number | string>>(10)
+
   const navigate = useNavigate()
   let state = {
     color: '#FF9595bb',
@@ -16,8 +17,7 @@ export default function Capture(): ReactElement {
     lazyRadius: 12,
   }
 
-  const dataRef = React.useRef(localStorage.getItem('drawing') || '{"lines":[]}')
-  const [cacheData, setCacheData] = React.useState(dataRef.current)
+  let canvasRef: CanvasDraw | null
 
   const handleCapture = (target: EventTarget & HTMLInputElement) => {
     setSource('')
@@ -31,41 +31,57 @@ export default function Capture(): ReactElement {
       }
     }
   }
-
+  const handleSliderChange = (event: any, newValue: number | number[]) => {
+    setBrushSize(newValue)
+  }
   return (
     <div className={classes.root}>
       <Typography variant="h2">Capture a picture</Typography>
       <br />
 
       {source && (
-        <CanvasDraw
-          className={classes.imgBox}
-          immediateLoading={true}
-          brushColor={state.color}
-          brushRadius={state.brushRadius}
-          lazyRadius={state.lazyRadius}
-          imgSrc={source}
-        />
-      )}
-
-      {!source && (
         <div>
-          <Typography variant="h3">Draw mask using your fingers</Typography>
+          <CanvasDraw
+            ref={canvasDraw => (canvasRef = canvasDraw)}
+            className={classes.imgBox}
+            immediateLoading={true}
+            brushColor={state.color}
+            brushRadius={typeof brushSize === 'number' ? brushSize : 10}
+            lazyRadius={state.lazyRadius}
+            imgSrc={source}
+          />
 
-          <div style={{ width: '50em' }}>
-            <Typography id="discrete-slider" gutterBottom>
+          <br />
+
+          <Typography variant="h3">Draw mask using your fingers</Typography>
+          <br />
+          <div style={{ width: '80vw', maxWidth: '50em', margin: 'auto', display: 'flex', alignItems: 'center' }}>
+            <Typography style={{ flex: '1' }} variant="h5" gutterBottom>
               Pen Size
             </Typography>
             <Slider
+              style={{ flex: '3' }}
               defaultValue={30}
               aria-labelledby="discrete-slider"
               valueLabelDisplay="auto"
               color="secondary"
+              value={typeof brushSize === 'number' ? brushSize : 10}
+              onChange={handleSliderChange}
               step={2}
               marks
               min={2}
               max={50}
             />
+
+            <IconButton
+              style={{ flex: '1' }}
+              color="primary"
+              onClick={() => {
+                canvasRef && canvasRef.clear()
+              }}
+            >
+              <DeleteSweep style={{ fontSize: '2em', margin: 0 }} />
+            </IconButton>
           </div>
         </div>
       )}
@@ -92,7 +108,10 @@ export default function Capture(): ReactElement {
         disabled={!source}
         style={{ marginLeft: '1em' }}
         onClick={() => {
-          navigate('/write-metadata')
+          const data = canvasRef && canvasRef.getSaveData()
+          console.log(data)
+
+          // navigate('/write-metadata')
         }}
       >
         Upload Image
